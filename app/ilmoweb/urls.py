f711537f5386b39d2f613b6600f08f6400f30080
Henri Immonen <henri.immonen000@gmail.com>
2023-09-16 13:33:59 +0300

    Simple login and logout page

--- app/ilmoweb/urls.py
+++ app/ilmoweb/urls.py
@@ -1,9 +1,10 @@
 """Module for routing urls."""
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
-    path("", views.home_page_view, name='home'),
-    path("database_test/", views.database_test_view, name="database_test")
+    path("", views.home_page_view, name="home"),
+    path("database_test/", views.database_test_view, name="database_test"),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
