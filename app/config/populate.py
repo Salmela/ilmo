1e320b9a868f4093d064cbdb0b25d3cec764e70c
ellaverak <ella.korkeaaho@gmail.com>
2023-09-18 20:56:57 +0300

    Add new test data

--- app/config/populate.py
+++ app/config/populate.py
@@ -9,16 +9,27 @@ sys.path.append('./')
 """
 import wsgi
 
-from ilmoweb.models import User
+from ilmoweb.models import User, Courses
 
 
 """
     Populates the relation connected to the User model with test data.
 
 """
-with open('config/data.csv') as file:
+with open('config/test_data_1.csv') as file:
     for line in file:
         line = line.replace('\n','')
         parts = line.split(';')
         user = User(student_id=parts[0], username=parts[1], password=parts[2], name=parts[3], surname=parts[4], email=parts[5])
         user.save()
+
+"""
+    Populates the relation connected to the Courses model with test data.
+
+"""
+with open('config/test_data_2.csv') as file:
+    for line in file:
+        line = line.replace('\n','')
+        parts = line.split(';')
+        course = Courses(name=parts[0], description=parts[1], labs_amount=parts[2], is_visible=parts[3])
+        course.save()
