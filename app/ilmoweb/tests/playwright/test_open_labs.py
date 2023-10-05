4b4f2b194af9353c352ef8b35e33f4afa2d98f89
Sohvi Puska <sohvip1@gmail.com>
2023-10-05 13:24:43 +0300

    playwright tests to open_labs.html

---
+++ app/ilmoweb/tests/playwright/test_open_labs.py
@@ -0,0 +1,16 @@
+import re
+from playwright.sync_api import Page, expect
+
+def test_has_title_and_heading(page: Page):
+    page.goto('/open_labs/')
+    expect(page).to_have_title(re.compile('Open Labs'))
+    assert page.inner_text('h1') == 'Fysikaalisen kemian laboratorio - Labra'
+
+def test_laboratoriotyöt_button(page: Page):
+    page.goto('/')
+    page.get_by_role('button', name='Kirjaudu sisään').click()
+    page.get_by_role('textbox', name='Username').fill('kemianopiskelija')
+    page.get_by_role('textbox', name='Password').fill('salasana123')
+    page.get_by_role('button', name='Log In').click()
+    page.get_by_role('link', name='laboratoriotyöt').click()
+    expect(page.get_by_role('heading', name='Tulevat labrat')).to_be_visible()
