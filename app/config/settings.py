6b09c0226362552e9d50d16a011275a86e5f510a
ellaverak <ella.korkeaaho@gmail.com>
2023-10-10 19:21:09 +0300

    simplify settings.py

--- app/config/settings.py
+++ app/config/settings.py
@@ -84,28 +84,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-"""if os.environ.get('GITHUB_WORKFLOW'):
-    DATABASES = {
-        'default': {
-           'ENGINE': 'django.db.backends.postgresql_psycopg2',
-           'NAME': 'github_actions',
-           'USER': 'postgres',
-           'PASSWORD': 'postgres',
-           'HOST': '127.0.0.1',
-           'PORT': '5432',
-        }
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': env("NAME"),
-            'USER': env("USER"),
-            'PASSWORD': env("PASSWORD"),
-            'HOST': env("HOST"),
-            'PORT': env("PORT"),
-        }
-    }"""
 DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
