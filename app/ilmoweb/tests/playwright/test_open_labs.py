7311f714005985746cb2fe0cbecca37ced6b18fe
Sohvi Puska <sohvip1@gmail.com>
2023-10-11 18:47:32 +0300

    improve playwright test

--- app/ilmoweb/tests/playwright/test_open_labs.py
+++ app/ilmoweb/tests/playwright/test_open_labs.py
@@ -3,11 +3,7 @@ from playwright.sync_api import Page, expect
 from helper_functions import login
 
 def test_has_title_and_heading(page: Page):
-    page.goto('/')
-    page.get_by_role('button', name='Kirjaudu sisään').click()
-    page.get_by_role('textbox', name='Username').fill('kemianopiskelija')
-    page.get_by_role('textbox', name='Password').fill('salasana123')
-    page.get_by_role('button', name='Log In').click()
+    login(page, 'kemianopiskelija', 'salasana123')
     page.goto('/open_labs/')
     expect(page).to_have_title(re.compile('Open Labs'))
     assert page.inner_text('h1') == 'Fysikaalisen kemian laboratorio - Labra'
