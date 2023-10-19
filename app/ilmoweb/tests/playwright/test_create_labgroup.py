00dfebf5696beb2e850b84209e791695d91b2e8d
Pinja <pinja.salomaki@gmail.com>
2023-10-19 16:21:51 +0300

    fix tests

--- app/ilmoweb/tests/playwright/test_create_labgroup.py
+++ app/ilmoweb/tests/playwright/test_create_labgroup.py
@@ -6,7 +6,7 @@ def test_login_and_navigate_to_create_labgroup(page: Page):
     login(page, 'kemianope', 'atomi123')
     page.goto('/created_labs')
     page.locator('[data-testid="group_1"]').click()
-    expect(page.get_by_text('Fysikaalisen kemian työt (osa 1)')).to_be_visible()
+    expect(page).to_have_title('Create group')
 
 
 def test_create_labgroup_and_check_that_it_is_created(page: Page):
@@ -18,7 +18,7 @@ def test_create_labgroup_and_check_that_it_is_created(page: Page):
     page.keyboard.press('Shift')
     page.get_by_test_id('time').select_option('12-16')
     page.get_by_test_id('place').select_option('D210 (Phy)')
-    page.locator('[data-testid="5"]').click()
+    page.locator('[data-testid="1"]').click()
     page.get_by_role('button', name='Luo labra').click()
     expect(page).to_have_title('Created labs')
     expect(page.get_by_text('31.10.2025')).to_be_visible()
