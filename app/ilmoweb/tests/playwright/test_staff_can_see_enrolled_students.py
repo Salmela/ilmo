38c609d00107c9fb059d664b3b2326d0b8855e2e
Sohvi Puska <sohvip1@gmail.com>
2023-11-06 21:09:56 +0200

    playwright fix v3

--- app/ilmoweb/tests/playwright/test_staff_can_see_enrolled_students.py
+++ app/ilmoweb/tests/playwright/test_staff_can_see_enrolled_students.py
@@ -9,5 +9,5 @@ def test_student_can_enroll_and_staff_sees_enrolled_student(page: Page):
     # page.get_by_role('link', name='Kirjaudu ulos').click()
     login(page, 'kemianope', 'atomi123')
     page.goto('/open_labs/')
-    page.locator('[data-testid="1"]').click()
-    expect(page.get_by_text('kemianopiskelija')).to_be_visible()
+    page.locator('[data-testid="11"]').click()
+    expect(page.get_by_text('Kurssin Kuningasvesi laboratoriotyöhön labra 1 ilmoittautuneet opiskelijat')).to_be_visible()
