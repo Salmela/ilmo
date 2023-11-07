a0ed11be419558ede7bae85df8f17d5f82037ff9
Sohvi Puska <sohvip1@gmail.com>
2023-11-07 15:19:42 +0200

    playwright fix 3

--- app/ilmoweb/tests/playwright/test_open_labs.py
+++ app/ilmoweb/tests/playwright/test_open_labs.py
@@ -6,7 +6,7 @@ def test_has_title_and_heading(page: Page):
     login(page, 'kemianopiskelija', 'salasana123')
     # page.goto('/open_labs/')
     expect(page).to_have_title(re.compile('Open Labs'))
-    assert page.inner_text('h1') == 'Fysikaalisen kemian'
+    assert page.inner_html('h1 b') == 'Fysikaalisen kemian<br>ilmoittautumisjärjestelmä'
 
 def test_laboratoriotyot_button(page: Page):
     login(page, 'kemianopiskelija', 'salasana123')
