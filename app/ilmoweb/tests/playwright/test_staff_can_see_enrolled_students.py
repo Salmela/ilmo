74cd7cc13e6e9963a4b0eff605829adfaf71ac4f
Sohvi Puska <sohvip1@gmail.com>
2023-11-09 16:54:32 +0200

    remove useless playwright test

--- app/ilmoweb/tests/playwright/test_staff_can_see_enrolled_students.py
+++ app/ilmoweb/tests/playwright/test_staff_can_see_enrolled_students.py
@@ -1,18 +1,6 @@
 from playwright.sync_api import Page, expect
 from helper_functions import login
 
-def test_student_can_enroll_and_staff_sees_enrolled_student(page: Page):
-    page.goto('/')
-    # login(page, 'kemianopiskelija', 'salasana123')
-    # page.get_by_role('link', name='Laboratoriotyöt').click()
-    # page.locator('[data-testid="group_1"]').click()
-    # page.get_by_role('link', name='Kirjaudu ulos').click()
-    login(page, 'kemianope', 'atomi123')
-    page.goto('/open_labs/')
-    page.locator('[data-testid="1"]').click()
-    expect(page.get_by_text('Ilmoittautuneet opiskelijat')).to_be_visible()
-
-
 def test_staff_does_not_see_student_who_cancelled_enrollment(page: Page):
     login(page, 'kemianopiskelija', 'salasana123')
     page.get_by_role('link', name='Laboratoriotyöt').click()
@@ -32,4 +20,3 @@ def test_staff_does_not_see_student_who_cancelled_enrollment(page: Page):
     login(page, 'kemianope', 'atomi123')
     page.goto('/open_labs/')
     expect(page.get_by_text('kemianopiskelija')).to_be_hidden()
-
