00a4bf05aeb44d8ff87108cf3d207bfeba6f47ab
Henri Immonen <henri.immonen000@gmail.com>
2023-11-15 19:58:36 +0200

    Change time picking to manual
    Fix unittests and replace single wuotes with double quotes
    
    Fix playwright tests and some refactoring

--- app/ilmoweb/logic/labgroups.py
+++ app/ilmoweb/logic/labgroups.py
@@ -9,16 +9,10 @@ def confirm(group_id):
     group.status = 2
     group.save()
 
-def create(lab, date, time, place, assistant):
+def create(lab, date, start_time, end_time, place, assistant): # pylint: disable=too-many-arguments
     """
         creates new labgroup in database
     """
-    if time == '8-12':
-        start_time = '08'
-        end_time = '12'
-    if time == '12-16':
-        start_time = '12'
-        end_time = '16'
 
     group = LabGroups(lab=lab,
                       date=date,
@@ -28,16 +22,10 @@ def create(lab, date, time, place, assistant):
                       assistant=assistant)
     group.save()
 
-def update(date, time, place, assistant, labgroup_id):
+def update(date, start_time, end_time, place, assistant, labgroup_id): # pylint: disable=too-many-arguments
     """
         updates labgroup in database
     """
-    if time == '8-12':
-        start_time = '08'
-        end_time = '12'
-    if time == '12-16':
-        start_time = '12'
-        end_time = '16'
 
     group = LabGroups.objects.get(pk=labgroup_id)
 
--- app/ilmoweb/tests/playwright/test_play.py
+++ app/ilmoweb/tests/playwright/test_play.py
@@ -4,18 +4,3 @@ from helper_functions import login
 def test_welcome_message(page: Page):
     page.goto('/')
     assert page.inner_html('h1 b') == 'Fysikaalisen kemian<br>ilmoittautumisjärjestelmä'
-
-# def test_login_link(page: Page):
-#     page.goto('/')
-#     page.get_by_role('link', name='Kirjaudu sisään').click()
-#     expect(page.get_by_role('heading', name='Log In')).to_be_visible()
-
-# def test_admin_view(page: Page):
-#     page.goto('/')
-#     login(page, 'kemianope', 'atomi123')
-#     assert page.inner_text('h6') == 'Olet admin roolissa'
-
-# def test_student_view(page: Page):
-#     page.goto('/')
-#     login(page, 'kemianopiskelija', 'salasana123')
-#     assert page.inner_text('h6') == 'Olet käyttäjäroolissa'
