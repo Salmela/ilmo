e218c7437ea24402389a828f245ab609dbbe9282
mirelle01 <lybackmirelle@gmail.com>
2023-12-01 13:29:10 +0200

    playwright fix

--- app/ilmoweb/tests/playwright/test_system.py
+++ app/ilmoweb/tests/playwright/test_system.py
@@ -4,18 +4,18 @@ from helper_functions import login
 def test_navigate_to_system_page_as_superuser(page: Page):
     page.goto('/')
     login(page, 'kemianope', 'atomi123')
-    page.get_by_test_id('system-button').click()
+    page.get_by_test_id('system_button').click()
     expect(page).to_have_title('System')
 
 def test_navigate_to_system_page_as_not_superuser(page: Page):
     page.goto('/')
     login(page, 'kemianopiskelija', 'salasana123')
-    expect(page.get_by_test_id('system-button')).to_be_hidden()
+    expect(page.get_by_test_id('system_button')).to_be_hidden()
 
 def test_update_message_to_students(page: Page):
     page.goto('/')
     login(page, 'kemianope', 'atomi123')
-    page.get_by_test_id('system-button').click()
+    page.get_by_test_id('system_button').click()
     expect(page.get_by_test_id('current_message')).to_have_text('Ei viestiä')
     page.get_by_test_id('message').fill('Uusi viesti')
     page.get_by_test_id('send_message').click()
