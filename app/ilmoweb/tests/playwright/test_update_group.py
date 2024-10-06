5197f4e043d5c353d289e43a4d3dd786b90dcf71
ellaverak <ella.korkeaaho@gmail.com>
2024-10-06 23:45:58 +0300

    playwright is driving me crazy

--- app/ilmoweb/tests/playwright/test_update_group.py
+++ app/ilmoweb/tests/playwright/test_update_group.py
@@ -18,7 +18,7 @@ def test_teacher_can_update_group(page: Page):
     page.get_by_test_id('end_time').fill('12:00')
     page.get_by_test_id('place').select_option('B152')
     page.get_by_test_id('assistant').select_option('kemianassari')
-    page.get_by_role('button', name='Päivitä labra').click()
+    page.get_by_role('button', name='Päivitä tiedot').click()
     expect(page).to_have_title(re.compile('Created labs'))
     expect(page.get_by_test_id('date_1')).to_have_text('11.11.2024 klo 8:00 - 12:00')
     expect(page.get_by_test_id('place_1')).to_have_text('B152')
